9bfcc49c8f30699bd811838f21f05a61430126b6
khe <khe@not.com>
2022-02-19 09:39:29 -0600

    first commit

---
+++ Untitled1.cpp
@@ -0,0 +1,56 @@
+class user 
+{ std:string status ="persona"
+  
+public:
+
+	std::string Nombres; 
+	std::string apellidos;
+	std::string direccion;
+	std::string tel;
+	std::string fecha_nacimiento;
+	std::string get_status();
+	{
+		return status:
+		
+		
+		
+	}
+	{
+		void set_status(std::string status)
+  }    
+  {
+  	if (status == "nit")
+  	status=status;
+  }
+  else
+{
+	status= "no status";
+}
+
+user (std::string Nombres, std::string apellidos, std::string direccion, std::string tel, std::string fecha_nacimiento)            )
+	{
+		Nombre=Nombre;
+		apellido=apellido;
+		direccion=direccion;
+		tel=tel;
+		fecha_nacimiento=fecha_nacimiento;		
+	}
+	~user ()
+	{
+		std::cout <<"Borrar\n";
+				
+	}		
+	
+};
+
+int add_user_if_not_exists(std::vector<user> &users,user User )
+int main()
+
+{
+	User user;
+	user.set_status("nit");
+	std::cout <<user.get_status() <<std::endl;
+
+	
+	
+}
